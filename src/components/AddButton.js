54912651c941ed2989afc6691f9b3eb9b0a561f8
Lucas Ferreira <lferreira@liveperson.com>
2020-05-07 00:13:47 +0200

    add button component

---
+++ src/components/AddButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddButton = () => {
+  return (
+    <div className="open-search">
+      <button>Add a book</button>
+    </div>
+  );
+};
+
+export default AddButton;
